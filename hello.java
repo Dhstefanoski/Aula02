c48e6f65fa63f5f70275c60c6b5096e076f67707
Dhstefanoski <dhstefanoski@minha.fag.edu.br>
2025-02-27 22:04:46 -0300

    Commit 1

---
+++ hello.java
@@ -0,0 +1,12 @@
+package aula02package;
+
+public class hello {
+
+	public static void main (String []args) {
+		helloWorld();
+	}
+	public static void helloWorld () {
+		System.out.println("Hello World");
+	
+ }
+}
